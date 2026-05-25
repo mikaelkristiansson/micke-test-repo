e7abbbc7daa22c30ebf5154c8361f0d40da43f46
Mikael Kristiansson <mikael.kristiansson@nordnet.se>
2026-05-25 10:51:25 +0200

    chore: move files

---
+++ deep/folders/with-long-name-for-testing/file-also-with-a-pretty-long-name.ts
@@ -0,0 +1,3 @@
+export function coolFunction(): string {
+    return "This is a cool function!";
+}
--- im-typescript.ts
+++ im-typescript.ts
@@ -3,3 +3,7 @@ type Name = string;
 export function greet(name: Name): string {
     return `Hello, ${name}!`;
 }
+
+export function farewell(name: Name): string {
+    return `Goodbye, ${name}!`;
+}
